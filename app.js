11f8b4e835470e57adcb8d538c7aa59220001b98
Rukhsar Ahmed <rukhsarnaich0@gmail.com>
2024-10-03 22:24:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,100 @@
+//JS Assignment 10
+
+//Task 01
+
+// var first_name = prompt("Enter your First name: ")
+// var last_name = prompt("Enter your Last name: ")
+
+// var Full_Name = (first_name +" "+ last_name)
+
+// alert("Hello! " + Full_Name + " Thanks you for coming to visit our webpage")
+
+//Task 02
+
+
+// var Phone_Model = prompt("What is your favorite mobile phone model?");
+
+// var length = Phone_Model.length;
+
+// if (Phone_Model !== null) {
+//     alert("The length of your input is: " + length);
+// } else {
+//     alert("No input provided.");
+// }
+
+//Task 03
+
+// var word = "Pakistani";
+
+// var letter = "n";
+
+// var index = word.indexOf(letter);
+
+// if (index !== -1) {
+//     alert("The index of letter " + letter + " in " + word + " is: " + index);
+// }
+
+
+//Task 04
+
+// var word = "Hello World";
+
+// var letter = "l";
+
+// var lastIndex = word.lastIndexOf(letter);
+
+// if (lastIndex !== -1) {
+//     alert("The last index of letter " + letter + " in " + word + " is: " + lastIndex);
+// }
+
+//Task 05
+
+// var word = "Pakistani";
+
+// var characterAtIndex = word.charAt(3);
+
+// alert("The character at index 3 in " + word + " is: " + characterAtIndex);
+
+//Task 06
+
+// var first_name = prompt("Enter your First name: ")
+// var last_name = prompt("Enter your Last name: ")
+
+// var Full_Name = first_name.concat(" ", last_name)
+
+// alert("Hello! " + Full_Name + " Thank you for coming to visit our webpage")
+
+//Task 07
+
+// var word = "Hyderabad";
+
+// console.log(word);
+
+// var replacedWord = word.replace("Hyder", "Islam");
+
+// console.log("The result after replacement is: " + replacedWord);
+
+//Task 08
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+
+// var replacedLetters = message.replace(/and/g ,"&")
+
+// alert(replacedLetters);
+
+//Task 09
+
+// var str = "472";
+
+// var num = Number(str);
+
+// console.log("Original String: " + str);
+// console.log("Converted Number: " + num);
+
+//Task 10
+
+// var userInput = prompt("Please enter something:");
+
+// var upperCaseInput = userInput.toUpperCase();
+
+// alert(upperCaseInput);
